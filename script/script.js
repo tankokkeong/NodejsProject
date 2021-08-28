cffc4bda301d814ffffddf76c68e6f95ec1a66bc
TAN KOK KEONG <tankokkeong4488@gmail.com>
2021-08-28 16:01:43 +0800

    view more comment

--- script/script.js
+++ script/script.js
@@ -1,20 +1,29 @@
-//Global variables
-var show = false;
+function viewMoreComment(container_id){
+  var comment_container = document.getElementById(container_id);
+  var comment_display_count = 0;
+  var view_more_trigger = document.getElementById("view-trigger-" + container_id);
+  var view_more_count = document.getElementById("view-more-" + container_id);
 
-$(document).ready(function(){
-  $("#show-toggle").click(function(){
+  for (var i = 0; i < comment_container.childNodes.length; i++) {
+    
+    if(comment_display_count < 5){
+      if(comment_container.childNodes[i].className == "comment-row"){
+        console.log("hi")
+        if(comment_container.childNodes[i].style.display == "none"){
+          //Display comment
+          comment_container.childNodes[i].style.display = "";
+          comment_display_count++;
+        }
+      }
+    }
+    
+  }
 
-    if(!show){
-        show = true;
-        $("#show-icon").removeClass("fa-angle-double-down");
-        $("#show-icon").addClass("fa-angle-double-up");
-    }
-    else
-    {
-        show = false;
-        $("#show-icon").removeClass("fa-angle-double-up");
-        $("#show-icon").addClass("fa-angle-double-down");
-    }
-    $("#create-post-form").slideToggle("slow");
-  });
-});
+  //Reduce view more count
+  if(parseInt(view_more_count.innerHTML) < 5){
+    view_more_trigger.style.display = "none";
+  }
+  else{
+    view_more_count.innerHTML = parseInt(view_more_count.innerHTML) - 5;
+  }
+}
