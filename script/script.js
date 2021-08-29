2b8ed37d4ac5d1262cc03405bc3ea624d7e7381b
TAN KOK KEONG <tankokkeong4488@gmail.com>
2021-08-29 08:32:47 +0800

    validation

--- script/script.js
+++ script/script.js
@@ -27,3 +27,57 @@ function viewMoreComment(container_id){
     view_more_count.innerHTML = parseInt(view_more_count.innerHTML) - 5;
   }
 }
+
+function postValidation(){
+
+  var author = document.getElementById("authorName");
+  var title = document.getElementById("title");
+  //Get CKE Editor value
+  var post_content = CKEDITOR.instances.content.getData();
+  var error_prompt = document.getElementsByClassName("error-prompt");
+  var error_count = 0;
+  
+  //Author name validation
+  if(author.value.trim().length == 0){
+    error_prompt[0].innerHTML = "Author name is required!";
+    error_count++
+  }
+  else{
+    error_prompt[0].innerHTML = "";
+  }
+
+  //Title validation
+  if(title.value.trim().length == 0){
+    error_prompt[1].innerHTML = "Post title is required!";
+    error_count++
+  }
+  else{
+    error_prompt[1].innerHTML = "";
+  }
+
+  if(post_content.trim().length == 0){
+    error_prompt[2].innerHTML = "Post content is required!";
+    error_count++
+  }
+  else{
+    error_prompt[2].innerHTML = "";
+  }
+
+  if(error_count == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function commentValidation(comment_id){
+  var comment_input = document.getElementById(comment_id);
+
+  if(comment_input.value.trim().length == 0){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
